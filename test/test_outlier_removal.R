3dd5f103a95d840d42ed33b324d2c651c4431621
Alex Labuda <alexlabuda@gmail.com>
2023-12-18 12:24:13 -0500

    add outlier testing script

---
+++ test/test_outlier_removal.R
@@ -0,0 +1,29 @@
+
+# Outlier Removal Tool ----------------------------------------------------
+
+# Libraries
+library(tidyverse)
+
+
+# Data --------------------------------------------------------------------
+
+# Load test transactions data
+
+transactions_tbl <- read_csv("test/data/test_transactions_data.csv") |> 
+  select(unique_transaction, visitor_id, transaction_revenue) |> 
+  mutate(transaction_revenue = transaction_revenue / 1000000)
+
+transactions_tbl |> glimpse()
+
+
+# Visualize outliers with boxplot
+transactions_tbl |> 
+  ggplot(aes(x = "", y = transaction_revenue)) +
+  geom_boxplot() +
+  labs(
+    title = "Transactions Revenue Boxplot",
+    x     = "",
+    y     = "Revenue"
+  ) +
+  coord_flip() +
+  theme_minimal()
