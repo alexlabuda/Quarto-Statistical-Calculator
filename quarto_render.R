42809bfbd4ae7d873f2dd8a83e787b43867f845e
Alex Labuda <alexlabuda@gmail.com>
2023-12-21 16:54:03 -0500

    test parameters

--- quarto_render.R
+++ quarto_render.R
@@ -5,25 +5,26 @@ library(quarto)
 document_name <- "post-ab-test-automation-test"
 
 # Define the path to your Quarto document
+# This is the path to the .qmd file
 quarto_file_path <- "01-statistical-test-report-automation.qmd"
 
-# Define the output directory
-output_dir <- "output_quarto_documents"
+# Define the output directory for the rendered document to be saved
+output_dir <- "output_quarto_documents/"
 
 # Define new parameter values here:
 new_params <- list(
   sample_size_A    = 9000,
-  success_A        = 1350,
+  success_A        = 1400,
   sample_size_B    = 9000,
   success_B        = 1450,
-  hypothesis       = "two.sided",
+  hypothesis       = "one.sided",
   confidence_level = 0.95
 )
 
 # Renders the document and saves to directory specified above
 quarto_render(
-  input         = quarto_file_path,
-  output_format = "html",
-  output_dir    = output_dir,
-  output_file   = paste0(document_name, ".html"),
+  input          = quarto_file_path,
+  output_format  = "html",
+  output_file    = paste0(document_name, ".html"),
+  execute_params = new_params
 )
